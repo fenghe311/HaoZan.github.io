69883e62d4c4d4d67d132d8c4a03bf43abb0959b
ray <haozanlawyer@gmail.com>
2023-03-26 09:57:48 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           includeCurrentVersion:false,
           routeBasePath: `/`,
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
